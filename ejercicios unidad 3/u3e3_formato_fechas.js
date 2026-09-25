f73ee990b33cf9b26ebdfabedcd76e8b81c0efed
Andrés Serena Quintilla <aserenaquintilla@gmail.com>
2026-09-25 13:24:28 +0200

    listo

--- ejercicios unidad 3/u3e3_formato_fechas.js
+++ ejercicios unidad 3/u3e3_formato_fechas.js
@@ -1,3 +1,26 @@
 // Ejercicio: u3e2_domingos
 // Autor: Andrés Serena Quintilla
 
+// 1. Declaramos la variable del dia de hoy
+const hoy = new Date();
+
+let dia = hoy.getDate();
+let mes = hoy.getMonth();
+let año = hoy.getFullYear();
+
+let formatoFecha = Number(prompt("Introduce 1 para ver la fecha así '25/09/2026', 2 para verlo así 'Viernes 25 de septiembre de 2026' o 3 para verlo al modo Americano 'Friday, September 25, 2026'."));
+
+switch (formatoFecha) {
+    case 1:
+        // aquí irá el formato 1
+        document.write(dia + "/" + (mes + 1) + "/" + año);
+        break;
+    case 2:
+        // aquí irá el formato 2
+        break;
+    case 3:
+        // aquí irá el formato 3
+        break;
+    default:
+        // aquí, si escribe otra cosa
+}

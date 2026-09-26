c28ef3864a3b8b7256ca9b91ae1f05ee19dd8cb8
Andrew <aserenaquintilla@gmail.com>
2026-09-26 10:43:59 +0200

    He terminado un ejercicio

--- ejercicios unidad 3/u3e3_formato_fechas.js
+++ ejercicios unidad 3/u3e3_formato_fechas.js
@@ -7,20 +7,42 @@ const hoy = new Date();
 let dia = hoy.getDate();
 let mes = hoy.getMonth();
 let año = hoy.getFullYear();
+let diaSemana = hoy.getDay();
 
-let formatoFecha = Number(prompt("Introduce 1 para ver la fecha así '25/09/2026', 2 para verlo así 'Viernes 25 de septiembre de 2026' o 3 para verlo al modo Americano 'Friday, September 25, 2026'."));
+let formatoFecha;
+
+do {
+    formatoFecha = Number(prompt("Introduce 1 para ver la fecha así '25/09/2026', 2 para verlo así 'Viernes 25 de septiembre de 2026' o 3 para verlo al modo Americano 'Friday, September 25, 2026'."));
+
+    if (formatoFecha !== 1 && formatoFecha !== 2 && formatoFecha !== 3) {
+        alert("Opción no válida, tienes que escribir 1, 2 o 3");
+    }
+} while (formatoFecha !== 1 && formatoFecha !== 2 && formatoFecha !== 3);
 
 switch (formatoFecha) {
     case 1:
-        // aquí irá el formato 1
-        document.write(dia + "/" + (mes + 1) + "/" + año);
+        // formato dd/mm/aaaa, el mes empieza en 0 así que le sumo 1
+        let mesActual = mes + 1;
+        if (mesActual < 10) {
+            mesActual = "0" + mesActual;
+        }
+        if (dia < 10) {
+            dia = "0" + dia;
+        }
+        document.write(dia + "/" + mesActual + "/" + año);
         break;
     case 2:
+        const diasDeLaSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+        const meses = ["enero", "febrero", "marzo","abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+        document.write(diasDeLaSemana[diaSemana] + ", " + dia + " de " + meses[mes] + " del " + año);
         // aquí irá el formato 2
         break;
     case 3:
-        // aquí irá el formato 3
+        const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        document.write(daysOfTheWeek[diaSemana] + ", " + months[mes] + " " + dia + ", " + año);
         break;
     default:
-        // aquí, si escribe otra cosa
+        document.write("Opción no válida, introduce 1, 2 o 3");// aquí, si escribe otra cosa
+        
 }
